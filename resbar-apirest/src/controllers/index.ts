e35bb285dde8700da6a178dab1c71560363fe8d5
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-09-12 21:33:57 -0300

    Product model, repository, controller and relationship with category

--- resbar-apirest/src/controllers/index.ts
+++ resbar-apirest/src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './ping.controller';
 export * from './category.controller';
+export * from './product.controller';
---
+++ resbar-apirest/src/models/product.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'object',
+    required: true,
+  })
+  category: object;
+
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
